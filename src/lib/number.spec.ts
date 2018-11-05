25238979f66ac3c9774405a12268d281d68dc565
Bram Borggreve <borggreve@gmail.com>
2018-11-05 01:54:56 -0500

    test: fix number test

--- src/lib/number.spec.ts
+++ src/lib/number.spec.ts
@@ -1,9 +1,13 @@
 // tslint:disable:no-expression-statement
 import test from 'ava'
-import { double, power } from './number'
+import { double, power, triple } from './number'
 
 test('double', t => {
   t.is(double(2), 4)
+})
+
+test('triple', t => {
+  t.is(triple(2), 6)
 })
 
 test('power', t => {
